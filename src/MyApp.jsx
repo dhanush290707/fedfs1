88bddde9e9856d4231b11209062c37abb4606050
dhanush290707 <dhanush290707@gmail.com>
2025-07-18 10:08:54 +0530

    v2

---
+++ src/MyApp.jsx
@@ -0,0 +1,14 @@
+import React, { Component } from 'react';
+import './MyApp.css';
+
+class MyApp extends Component {
+    render() {
+        return (
+            <div>
+                <label className='myStyle'>KL University</label>
+            </div>
+        );
+    }
+}
+
+export default MyApp;
